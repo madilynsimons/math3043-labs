d70f55bf182d22938a170d10f2350a34c34fc872
Madilyn Simons <madilynsimons@gmail.com>
2018-09-24 07:35:16 -0400

    Lab2Simons docs

--- lab2/Lab2Simons.m
+++ lab2/Lab2Simons.m
@@ -2,6 +2,8 @@
 %  @author Madilyn Simons 
 
 %% Problem 1
+% Perform four different fixed-point
+% iteration methods of computing 25^(1/3)
 
 fixedPointIterationA();
 fixedPointIterationB();
@@ -13,6 +15,8 @@ fprintf('B, D, A\n');
 fprintf('NB: Method C does not converge to a sufficient approximation\n\n');
 
 %% Problem 2
+% Use Newton's Method to find an approximation of
+% 25^(1/3) correct to within 10^(-10).
 
 NewtonsMethod();
 
@@ -21,13 +25,29 @@ fprintf('Newtons Method takes 9 iterations to approximate 25^(1/3)\n');
 fprintf('Newtons method approximates 25^(1/3) %.f%% faster than the Bisection method\n\n', 100*(31-9)/31);
 
 %% Problem 3
+% Use Newton's method to find the time, accurate to within 10^(-5) s
+% that it takes for an object falling from 300 ft to hit the ground.
 
-FallingObject();
+t = FallingObject();
+
+fprintf('\nThe object takes %.5f seconds to hit the ground\n', t);
 
 %% Problem 4
+% Use Newton's method to calculate how much of a drug should be administered
+% to a patient given the maximum safe concentration and equation for
+% concentration of drug in the bloodstream, how long it takes to reach
+% that concentration, and when to administer a second dose.
 
-MaxDrugConcentration();
+[hours, minutes] = MaxDrugConcentration();
+
+fprintf('\n%f units of medicine should be injected to reach\n', exp(1)/3);
+fprintf('the max safe concentration after %d hours.\n', 3);
+fprintf('The second injection should be injected\n');
+fprintf('%d hours and %d minutes later.\n', hours, minutes);
 
 %% Problem 5
+% Use Newton's method to approximate the zero of the function
+% f(x) = x^2 - 2xe^(-x) + e^(-2x) to within 10^(-8)
 
-NewtonsMethod2();
+x = NewtonsMethod2();
+fprintf('\nx = %.8f\n', x);
