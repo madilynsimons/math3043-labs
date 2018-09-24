aac61d08e5c8084c357aff358b8db35aa66cb6e3
Madilyn Simons <madilynsimons@gmail.com>
2018-09-24 06:39:42 -0400

    Added doc to fixedPointIterationC

--- lab2/fixedPointIterationC.m
+++ lab2/fixedPointIterationC.m
@@ -1,23 +1,35 @@
 function fixedPointIterationC()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% Use the following fixed-point iteration method to compute 21^(1/3)
+% pn = pn-1 - ((pn-1)^4 - 21(pn-1))/((pn-1)^2 - 21)
 
+% how many iterations it takes to compute 21^(1/3) using this method
 how_many_iterations = 0;
 
+% pn-1
 p_last = 0;
+
+% pn
 p_current = 1;
 
+% tolerance
 TOL = 10^(-10);
 
+% Until the error range is less than tolerance,
+% we continue to apply this fixed-point iteration method.
 while abs(p_current - p_last) >= TOL
     
+    % set pn-1 to the last value of pn
     p_last = p_current;
     
+    % calculate pn using the fixed-point iteration method
     p_current = p_last - (p_last^4 - 21*p_last)/(p_last^2 - 21);
     
+    % increment how_many_iterations
     how_many_iterations = how_many_iterations + 1;
+
 end
 
+% print the results
 fprintf("n: %d\t", how_many_iterations);
 fprintf("p%d:   %.10f\t", how_many_iterations, p_current);
 fprintf("|error|: %.10f\n", TOL);
