ea5344e552d014f53e535f42640e976348ad1c0e
Madilyn Simons <madilynsimons@gmail.com>
2018-10-11 15:31:59 -0400

    Fixed logic errors in divided_difference.m

--- lab5/divided_difference.m
+++ lab5/divided_difference.m
@@ -7,30 +7,34 @@ function f = divided_difference(x, y, n)
 % outputs:
 % f - array of values f[x0, x1, ... , xi] for i = 0, 1, 2, ..., n
 
+x = [0.0 0.1 0.3 0.6 1.0];
+y = [-6.00000 -5.89483 -5.65014 -5.17788 -4.28172];
+n = 5;
+
 % initialize F
 % F is a that will hold values for Newton's Divided Difference
 F = ones(n, n);
 
 % set Fi to y(i) for i=1,2,3,...,n
-for i=0:n
-    F(i, n) = y(i);
+for i=1:n
+    F(i, 1) = y(i);
 end
 
 % apply Newton's divided difference
 for i=2:n
     for j=2:i
         numerator = F(i, j-1) - F(i-1, j-1);
-        denominator = x(i) - x(i-j);
+        denominator = x(i) - x(i - j + 1);
         F(i, j) = numerator/denominator;
     end
 end
 
 % initialize f
-f = ones(1, n);
+f = ones(1, n-1);
 
 % set f to F(i, i)
-for i=1:n
-    f(i) = F(i, i);
+for i=2:n
+    f(i-1) = F(i, i);
 end
 
 end
