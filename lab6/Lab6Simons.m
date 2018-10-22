828b5310ab617fdf4c514fe39226ab86db1feccd
Madilyn Simons <madilynsimons@gmail.com>
2018-10-22 02:26:17 -0400

    Problem 3 Lab 6

--- lab6/Lab6Simons.m
+++ lab6/Lab6Simons.m
@@ -64,3 +64,47 @@ hold off
 
 
 %% Problem 3
+
+n = 5;
+x = [0.0 0.25 0.5 1.0 1.25]; % miles
+a = [0.0 23.04 47.37 97.45 123.66]; % time in seconds
+
+[a, b, c, d] = natural_cubic_spline(n, x, a);
+
+x1 = 0.0:0.01:0.25;
+y1 = polynomial(x1, a(1), b(1), c(1), d(1), x(1));
+
+x2 = 0.25:0.01:0.5;
+y2 = polynomial(x2, a(2), b(2), c(2), d(2), x(2));
+
+x3 = 0.5:0.01:1.0;
+y3 = polynomial(x3, a(3), b(3), c(3), d(3), x(3));
+
+x4 = 1.0:0.01:1.25;
+y4 = polynomial(x4, a(4), b(4), c(4), d(4), x(4));
+
+figure();
+hold on
+plot(x1, y1, 'blue');
+plot(x2, y2, 'blue');
+plot(x3, y3, 'blue');
+plot(x4, y4, 'blue');
+hold off
+
+time = y3(26);
+minutes = 1;
+seconds = time - 60;
+
+fprintf('According to the natural cubic spline, we estimate that\n');
+fprintf('California Chromes time at the three-quarter-mile pole was\n');
+fprintf('%d minutes and %.2f seconds.\n\n', minutes, seconds);
+
+fprintf('California Chromes actual time was 1 minute and 11.8 seconds.\n\n');
+
+fprintf('Our estimate was off by %.2f seconds.\n\n', seconds - 11.8);
+
+diff_start = (diff(x1) ./ diff(y1)) * 3600; % delta distance over delta time
+diff_end   = (diff(x4) ./ diff(y4)) * 3600; % delta distance over delta time
+
+fprintf('Estimated starting speed:\t %.2f miles per hour\n', diff_start(1));
+fprintf('Estimated ending speed:\t\t %.2f miles per hour\n', diff_end(25));
