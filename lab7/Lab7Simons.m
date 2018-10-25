5035c606ef75ba16f222809e6b6f200bbb4ba6cd
Madilyn P Simons <madilynsimons@gmail.com>
2018-10-25 15:50:55 -0400

    Problem 2 for Lab 7

--- lab7/Lab7Simons.m
+++ lab7/Lab7Simons.m
@@ -31,3 +31,47 @@ fprintf('Approximation using Composite Simpsons Rule\n');
 fprintf('%f\n\n', s_approx);
 
 %% Problem 2
+%  Use the Composite Trapezoidal rule to approximate the area averaged
+%  lining temperature of a brake pad
+
+% a series of radii on the pad-disk
+r = [0.308 0.325 0.342 0.359 0.376 0.393 0.410 0.427 0.444 0.461 0.478];
+
+% the temperature of the pad disk at radius r(i)
+t = [640 794 885 943 1034 1064 1114 1152 1204 1222 1239];
+
+% the angle subtended by the sector brake pads
+theta_p = 0.7051;
+
+% find the sum of the inner nodes of T(r)*r*theta
+sum = 0;
+for i=2:10
+    sum = sum + t(i) * r(i) * theta_p;
+end
+
+% the sum of the outter nodes of T(r)*r*theta
+k = theta_p * (t(1) * r(1) + t(11) * r(11));
+
+% an approximate value of the integral of T(r)*r*theta from re to ro
+top = (0.0170 / 2.0) * (2 * sum + k);
+
+% the sum of the inner nodes of r*theta
+sum = 0;
+for i=2:10
+    sum = sum + r(i) * theta_p;
+end
+
+% the sum of the outter nodes of r*theta
+k = theta_p * (r(1) + r(11));
+
+% an approximate value of the integral of r*theta from re to ro
+bottom = (0.0170 / 2.0) * (2 * sum + k);
+
+% approximate value of T
+T = top / bottom;
+
+% print the result
+fprintf('T = %.3f\n', T);
+
+
+%% Problem 3
