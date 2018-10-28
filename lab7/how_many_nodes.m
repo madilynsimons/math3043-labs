8015a08322e60550d22004aab6291bd6bf92b5d8
Madilyn Simons <madilynsimons@gmail.com>
2018-10-28 17:37:42 -0400

    Finished commenting how_many_nodes

--- lab7/how_many_nodes.m
+++ lab7/how_many_nodes.m
@@ -1,9 +1,25 @@
 function n = how_many_nodes(fun, a, b, max_error)
+%  Find how many nddes needed to approximate the integral of a
+%  function within a max error
+%
+%  Input:
+%   fun       - function to approximate integral of
+%   a, b      - end points of integral
+%   max_error - maximum error bound
+%
+%  Output:
+%    n - number of nodes needed
 
-n = 4;
+% min number of nodes is 4.  set to 2 so the loop will set it to 4
+n = 2;
+
+% (b - a) / 180
 k = (b - a) / 180.0;
+
+% set current error to max error at start of loop
 error = max_error;
 
+% check to see if we're under max error
 while error >= max_error
     
     % update n
@@ -15,12 +31,12 @@ while error >= max_error
    % create a linespace in whic values are h numbers apart
    x = a:h:b;
    
-   % find the 4 derivative of the function
-   f = fun(x);
-   
+   % dx
    d = diff(x);
    diff_x = d(1);
    
+   % find the 4 derivative of the function
+   f = fun(x);
    df = diff(f) ./ diff_x;
    d2f = diff(df) ./ diff_x;
    d3f = diff(d2f) ./ diff_x;
