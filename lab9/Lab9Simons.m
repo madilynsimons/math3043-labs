1aea9112e8827b023681e5137173a28cbc347b7e
Madilyn Simons <madilynsimons@gmail.com>
2018-11-08 19:53:56 -0500

    Starting problem 2 Lab 9

--- lab9/Lab9Simons.m
+++ lab9/Lab9Simons.m
@@ -31,4 +31,25 @@ end
 %% Problem 2
 %  Implement Gaussian elimination with scaled partial pivoting to solve
 %  the following linear system:
-%
+%   ?x1 + ?2x2 -   x3 +   x4 = 0
+%   ex1 -   x2 +   x3 +  2x4 = 1
+%    x1 +   x2 - ?3x3 +   x4 = 2
+%   -x1 -   x2 +   x3 - ?5x4 = 3
+
+% the number of equations / unknowns
+n = 4;
+
+% the linear system represented by an augmented matrix
+A = [ pi     sqrt(2)       -1      1  0; ...
+      exp(1)     -1        1       2  1; ...
+      1           1  -sqrt(3)      1  2; ...
+     -1          -1        1 -sqrt(5) 3];
+
+% use Gaussian elimination with scaled partial pivoting to solve the linear
+% system
+x = ScaltedPartialPivoting(n, A);
+
+% print the results
+for i=1:n
+    fprintf('x(%d) = %f\n', i, x(i));
+end
