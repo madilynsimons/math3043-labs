822a867de50fa13516a5bdbd950b13eb26dc26cf
Madilyn Simons <madilynsimons@gmail.com>
2018-11-08 20:14:40 -0500

    Get pivot for scaled partial pivoting function'

---
+++ lab9/getPivotScaled.m
@@ -0,0 +1,25 @@
+function p = getPivotScaled(A, s, n, i)
+
+rhs = intmin;
+
+for j=1:n
+    if abs(A(NROW(j), i)) / s(NROW(j)) > rhs
+        rhs = abs(A(NROW(j), i)) ./ s(NROW(j));
+    end
+end
+
+p = -1;
+
+for it=i:n
+    lhs = abs(A(NROW(it), i)) / s(NROW(it));
+    if lhs == rhs
+        p = it;
+    end
+end
+
+if rhs == intmin || p == -1
+    fprintf('Warning: Pivot not found for i = %d\n', i);
+end
+
+end
+
