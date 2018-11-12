3680d595b02775b12f403a2ce0dce2442963f944
Madilyn Simons <madilynsimons@gmail.com>
2018-11-12 09:07:25 -0500

    getPivot comments

--- lab9/getPivot.m
+++ lab9/getPivot.m
@@ -1,24 +1,37 @@
 function p = getPivot(A, n, i, NROW)
-
 % Let p be the smallest integer such that i <= p <= n
-% AND |a(NROW(p), i)| = max (|a(NROW(j), i) |) j i <= j <= n
+% AND |a(NROW(p), i)| = max (|a(NROW(j), i) |) i <= j <= n
+% Input: A - linear system
+%        n, i - lower and upper bounds of p
+%        NROW - row pointers
+%
+% Output: p - pivot
     
+% initialize max (|a(NROW(j), i) |) j i
 max_a = intmin;
+
+% initialize pivot
 p = intmax;
 
+% find max (|a(NROW(j), i) |)
 for j=i:n
+    % if (|a(NROW(j), i) |)is larger than max_a, update max_a
     if abs(A(NROW(j), i)) > max_a
         max_a = abs(A(NROW(j), i));
     end
 end
 
+% find pivot
 for it=i:n
+    % the pivot is the first instance in which 
+    % |a(NROW(p), i)| = max (|a(NROW(j), i) |) 
     if abs(A(NROW(it), i)) == max_a
         p = it;
         return
     end
 end
 
+% error message
 if p == intmax || max_a == intmin
     disp('WARNING: PIVOT NOT FOUND');
 
